4711a5b9571c722ade8c162d16fb9c0017a00353
Mhd Adnan Kassoumeh <kasmh@odoo.com>
2026-03-17 03:41:07 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'Odoo Technical Course',
+  description: 'TCO exercises repo',
+  base: '/odoo-technical-course/', // Required for GitHub Pages
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/your-username/odoo-technical-course' }
+    ],
+    sidebar: [
+      '/',
+      '/guide/'
+    ]
+  }
+}
